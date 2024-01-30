e52ead902ee9af8af8e3503f3b79c03578ff6625
LuisFerArredondo <luisfernando.arredondo@gmail.com>
2024-01-30 13:32:38 -0500

    Advantage Kit Working

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "RobotCode24_2706";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "8cf83adf371ec97bfeb814fdb62cb3fa4504a489";
-  public static final String GIT_DATE = "2024-01-30 13:16:41 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "cb019c9e42c4ae05126ae915ea3f34fcbf768e52";
+  public static final String GIT_DATE = "2024-01-30 13:21:39 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-30 13:20:34 EST";
-  public static final long BUILD_UNIX_TIME = 1706638834710L;
+  public static final String BUILD_DATE = "2024-01-30 13:26:06 EST";
+  public static final long BUILD_UNIX_TIME = 1706639166100L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
